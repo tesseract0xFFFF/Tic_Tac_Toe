9834f1363282d14d11d13654848e777d074abf40
tesseractx0FFFF <itkugit321@gmail.com>
2023-05-07 15:15:55 +0300

    Created a grid, each cell in the grid is bound to its respective gameBoard object. Added an event listener to each cell such that when clicked, the respective gameBoard object's 'occupied' property will change. In addition, every gameBoard object now has a domElement propery that contains a reference to its respective dom element object.

--- js/app.js
+++ js/app.js
@@ -5,8 +5,38 @@ const gameBoard = (() => {
   for (let i = 0; i < (rows); i += 1) {
     boardArray[i] = [];
     for (let j = 0; j < (columns); j += 1) {
-      boardArray[i][j] = { occupied: false, domElement: '' };
+      boardArray[i][j] = { occupied: false, mark: '', domElement: '' };
     }
   }
-  return { boardArray };
+
+  // what happens after a player clicks on a cell.
+  const cellClick = (obj) => {
+    if (obj.occupied === false) {
+      obj.occupied = true;
+    }
+  };
+
+  // goes over objects in the array and binds
+  // them to their respective div elements
+  const elementGenerator = () => {
+    for (let i = 0; i < (rows); i += 1) {
+      for (let j = 0; j < (columns); j += 1) {
+        const objectInArray = boardArray[i][j];
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('cell');
+        // addEventListener uses its default 'this' value so i could not rely on 'this'
+        // to reference the object in the array instead, i used a closure.
+        cellElement.addEventListener('click', () => {
+          cellClick(objectInArray);
+        });
+        objectInArray.domElement = cellElement;
+        const tictactoeGrid = document.querySelector('.tictactoeGrid');
+        tictactoeGrid.appendChild(cellElement);
+      }
+    }
+  };
+
+  return { boardArray, elementGenerator };
 })();
+
+gameBoard.elementGenerator();
