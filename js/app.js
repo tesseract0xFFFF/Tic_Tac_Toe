0f1115d156f9f6811bf657e3c510d5efb27487dc
tesseractx0FFFF <itkugit321@gmail.com>
2023-05-15 21:46:17 +0300

    Added a gameController function. Clicking on cells will now cause each player's mark to show inside of them and will switch to the other player's turn. Also added a small indicator of which turn it is.

--- js/app.js
+++ js/app.js
@@ -9,34 +9,71 @@ const gameBoard = (() => {
     }
   }
 
-  // what happens after a player clicks on a cell.
-  const cellClick = (obj) => {
-    if (obj.occupied === false) {
-      obj.occupied = true;
-    }
+  return {
+    boardArray, rows, columns,
   };
-
-  // goes over objects in the array and binds
-  // them to their respective div elements
-  const elementGenerator = () => {
-    for (let i = 0; i < (rows); i += 1) {
-      for (let j = 0; j < (columns); j += 1) {
-        const objectInArray = boardArray[i][j];
-        const cellElement = document.createElement('div');
-        cellElement.classList.add('cell');
-        // addEventListener uses its default 'this' value so i could not rely on 'this'
-        // to reference the object in the array instead, i used a closure.
-        cellElement.addEventListener('click', () => {
-          cellClick(objectInArray);
-        });
-        objectInArray.domElement = cellElement;
-        const tictactoeGrid = document.querySelector('.tictactoeGrid');
-        tictactoeGrid.appendChild(cellElement);
-      }
-    }
-  };
-
-  return { boardArray, elementGenerator };
 })();
 
-gameBoard.elementGenerator();
+// player factory function.
+const player = (name, mark) => {
+  const getName = () => name;
+  const getMark = () => mark;
+  return { getMark, getName };
+};
+
+const gameController = () => {
+  const player1 = player('Player1', 'x');
+  const player2 = player('Player2', 'o');
+
+  const players = [player1, player2];
+  let currentPlayer = players[0];
+
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    const currentPlayerDisplay = document.querySelector('.currentPlayer');
+    currentPlayerDisplay.textContent = `${currentPlayer.getName()}'s turn`;
+  };
+
+  const getCurrentPlayer = () => currentPlayer;
+
+  // Playes a round.
+  const playRound = (obj) => {
+    const currentCell = obj;
+    if (obj.occupied === false) {
+      currentCell.occupied = true;
+      currentCell.mark = currentPlayer.getMark();
+      currentCell.domElement.textContent = `${currentCell.mark}`;
+      switchPlayer();
+    }
+  };
+  return {
+    playRound, player1, player2,
+  };
+};
+
+// renders board display.
+const displayControl = (() => {
+  // creates a grid and appends an indicator of the current player.
+  const container = document.querySelector('.container');
+  const tictactoeGrid = document.querySelector('.tictactoeGrid');
+  const currentPlayerDisplay = document.querySelector('.currentPlayer');
+  container.appendChild(currentPlayerDisplay);
+
+  const game = gameController();
+
+  for (let i = 0; i < (gameBoard.rows); i += 1) {
+    for (let j = 0; j < (gameBoard.columns); j += 1) {
+      const objectInArray = gameBoard.boardArray[i][j];
+      const cellElement = document.createElement('div');
+      cellElement.classList.add('cell');
+      // addEventListener uses its default 'this' value so i could not rely on 'this'
+      // to reference the object in the array. Instead, i used a closure.
+      cellElement.addEventListener('click', () => {
+        game.playRound(objectInArray);
+      });
+      objectInArray.domElement = cellElement;
+      tictactoeGrid.appendChild(cellElement);
+    }
+  }
+  return { currentPlayerDisplay };
+})();
