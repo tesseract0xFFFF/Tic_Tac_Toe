ce1d01b3548839c5b1af22059f30bcfe21fe3e6d
tesseractx0FFFF <itkugit321@gmail.com>
2023-05-19 14:46:19 +0300

    Fixed a problem in which the occupiedCells array did not clear after a reset. Fixed a problem in which cpuTurn cell lookup got stuck in an infinite loop if all cells were occupied. Fixed another problem in which the the cell counter did not clear up after restarting the game.

--- js/app.js
+++ js/app.js
@@ -2,19 +2,19 @@ const gameBoard = (() => {
   const rows = 3;
   const columns = 3;
   const boardArray = [];
+  // keeps track of occupied cells.
+  const occupiedCells = [];
 
   for (let i = 0; i < (rows); i += 1) {
     boardArray[i] = [];
     for (let j = 0; j < (columns); j += 1) {
-      // Initializing mark properties with asterisks.
-      // This type of padding will be important when checking for wins.
-      boardArray[i][j] = { occupied: false, mark: '*', domElement: '' };
+      boardArray[i][j] = { occupied: false, mark: '', domElement: '' };
     }
   }
 
-  const currentBoardState = []; // used to determine whether someone has won or not.
+  // const currentBoardState = []; // used to determine whether someone has won or not.
   return {
-    boardArray, rows, columns, currentBoardState,
+    boardArray, rows, columns, occupiedCells,
   };
 })();
 
@@ -25,18 +25,16 @@ const player = (name, mark) => {
   return { getMark, getName };
 };
 
+// keeping track of occupied cells.
+const countingCells = () => {
+  let occupiedCellCount = 0;
+  const incrementCellCount = () => { occupiedCellCount += 1; };
+  const resetCellCount = () => { occupiedCellCount = 0; };
+  return { incrementCellCount, resetCellCount };
+};
+
 const gameController = () => {
   const player1 = player('Player1', 'x');
-  const occupiedCells = [];
-
-  // keeping track of occupied cells.
-  const countingCells = () => {
-    let occupiedCellCount = 0;
-    const incrementCellcount = () => occupiedCellCount++;
-    const resetCellCount = () => { occupiedCellCount = 0; };
-    return { incrementCellcount, occupiedCellCount, resetCellCount };
-  };
-
   const cellCount = countingCells();
   // generates a pseudo-random number from 0 to 2
   const cpuRandomChoice = () => Math.floor(Math.random() * 3);
@@ -54,14 +52,19 @@ const gameController = () => {
     const cpuMark = 'o';
     let randomCell = getRandomCell();
     // checks whether reandomCell is already occupied or not.
-    while (occupiedCells.includes(randomCell)) {
+    // checks if all cells are already occupied.
+    if (gameBoard.occupiedCells.length === 9) {
+      alert('Tie!');
+      return;
+    }
+    while (gameBoard.occupiedCells.includes(randomCell)) {
       randomCell = getRandomCell();
     }
     randomCell.occupied = true;
-    occupiedCells.push(randomCell);
+    gameBoard.occupiedCells.push(randomCell);
     randomCell.mark = cpuMark;
     randomCell.domElement.textContent = `${randomCell.mark}`;
-    cellCount.incrementCellcount();
+    cellCount.incrementCellCount();
   };
 
   const restartGame = () => {
@@ -72,72 +75,73 @@ const gameController = () => {
         // This type of padding will be important when checking for wins.
         gameBoard.boardArray[i][j].domElement.textContent = '';
         gameBoard.boardArray[i][j].occupied = false;
-        gameBoard.boardArray[i][j].mark = '*';
+        gameBoard.boardArray[i][j].mark = '';
       }
     }
+    gameBoard.occupiedCells.length = 0;
     const announcement = document.querySelector('.announcer');
     announcement.textContent = '';
   };
 
   // checks for a win.
-  const winCheck = () => {
-    const player1Wins = ['xxx******', '***xxx***', '******xxx', 'x**x**x**', '*x**x**x*', '**x**x**x', 'x***x***x', '**x*x*x**'];
-    const cpuWins = ['ooo******', '***ooo***', '******ooo', 'o**o**o**', '*o**o**o*', '**o**o**o', 'o***o***o', '**o*o*o**'];
-    gameBoard.boardArray.forEach((subArray) => {
-      subArray.forEach((cell) => { gameBoard.currentBoardState.push(cell.mark); });
-    });
-    const boardStateConvertedToString = gameBoard.currentBoardState.join('');
-    // the arrays above do not contain every winning map in existence. i am only
-    // looking for 8 states for each player.
-    const player1WinState = boardStateConvertedToString.replaceAll('o', '*');
-    const cpuWinState = boardStateConvertedToString.replaceAll('o', '*');
-    // currentBoardState is rendered every time winCheck is invoked.
-    gameBoard.currentBoardState.length = 0;
-    // checks if player 1 won or not.
-    if (player1Wins.includes(player1WinState)) {
-      alert('player1 wins!');
-      return 0;
-    }
+  // const winCheck = () => {
+  //   const player1Wins = ['xxx******', '***xxx***', '******xxx', 'x**x**x**', '*x**x**x*', '**x**x**x', 'x***x***x', '**x*x*x**'];
+  //   const cpuWins = ['ooo******', '***ooo***', '******ooo', 'o**o**o**', '*o**o**o*', '**o**o**o', 'o***o***o', '**o*o*o**'];
+  //   gameBoard.boardArray.forEach((subArray) => {
+  //     subArray.forEach((cell) => { gameBoard.currentBoardState.push(cell.mark); });
+  //   });
+  //   const boardStateConvertedToString = gameBoard.currentBoardState.join('');
+  //   // the arrays above do not contain every winning map in existence. i am only
+  //   // looking for 8 states for each player.
+  //   const player1WinState = boardStateConvertedToString.replaceAll('o', '*');
+  //   const cpuWinState = boardStateConvertedToString.replaceAll('o', '*');
+  //   // currentBoardState is rendered every time winCheck is invoked.
+  //   gameBoard.currentBoardState.length = 0;
+  //   // checks if player 1 won or not.
+  //   if (player1Wins.includes(player1WinState)) {
+  //     alert('player1 wins!');
+  //     return 0;
+  //   }
 
-    if (cpuWins.includes(cpuWinState)) {
-      alert('cpuWins wins!');
-      return 1;
-    }
+  //   if (cpuWins.includes(cpuWinState)) {
+  //     alert('cpuWins wins!');
+  //     return 1;
+  //   }
 
-    if (cellCount.occupiedCellCount === 9) {
-      alert('tie!');
-      return 2;
-    }
+  //   if (cellCount.occupiedCellCount === 9) {
+  //     alert('tie!');
+  //     return 2;
+  //   }
 
-    return false;
-  };
+  //   return false;
+  // };
 
   // Playes a round.
   const playRound = (obj) => {
     const currentCell = obj;
     if (obj.occupied === false) {
       currentCell.occupied = true;
-      occupiedCells.push(currentCell);
+      gameBoard.occupiedCells.push(currentCell);
       currentCell.mark = player1.getMark();
       currentCell.domElement.textContent = `${currentCell.mark}`;
-      cellCount.incrementCellcount();
+      cellCount.incrementCellCount();
       // check for a win or a tie.
-      const winOrNot = winCheck();
-      if (winOrNot === 0) {
-        const announcement = document.querySelector('.announcer');
-        announcement.textContent = 'Player1 has won!';
-        return;
-      }
-      if (winOrNot === 1) {
-        const announcement = document.querySelector('.announcer');
-        announcement.textContent = 'cpu has won!';
-        return;
-      }
-      if (winOrNot === 2) {
-        const announcement = document.querySelector('.announcer');
-        announcement.textContent = 'tie!';
-        return;
-      }
+      // const winOrNot = winCheck();
+      // if (winOrNot === 0) {
+      //   const announcement = document.querySelector('.announcer');
+      //   announcement.textContent = 'Player1 has won!';
+      //   return;
+      // }
+      // if (winOrNot === 1) {
+      //   const announcement = document.querySelector('.announcer');
+      //   announcement.textContent = 'cpu has won!';
+      //   return;
+      // }
+      // if (winOrNot === 2) {
+      //   const announcement = document.querySelector('.announcer');
+      //   announcement.textContent = 'tie!';
+      //   return;
+      // }
       cpuTurn();
     }
   };
@@ -148,17 +152,17 @@ const gameController = () => {
 
 // renders board display.
 const displayControl = (() => {
+  const game = gameController();
   // creates a grid and appends an indicator of the current player.
   const container = document.querySelector('.container');
   const tictactoeGrid = document.querySelector('.tictactoeGrid');
   const currentPlayerDisplay = document.querySelector('.currentPlayer');
   container.appendChild(currentPlayerDisplay);
 
+  // creates a restart buttons and attackes an event listener to it.
   const gameControl = gameController();
   const restartButton = document.querySelector('.restart');
-  restartButton.addEventListener('click', gameControl.restartGame);
-
-  const game = gameController();
+  restartButton.addEventListener('click', game.restartGame);
 
   for (let i = 0; i < (gameBoard.rows); i += 1) {
     for (let j = 0; j < (gameBoard.columns); j += 1) {
